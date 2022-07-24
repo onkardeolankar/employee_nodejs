ab302ad1577be3e80d90ac16d84ddec771b21e85
onkardeolankar1996@gmail.com <codewithkeeda@gmail.com>
2022-07-24 10:31:38 +0530

    first commit

---
+++ employee.js
@@ -0,0 +1,42 @@
+[
+    {
+        "id": "1",
+        "firstName": "Bhavesh",
+        "lastName" : "Parab",
+        "email" : "bhavesh@gmail.com",
+        "salary" : "250000",
+        "date" : "2020-04-14"
+    },
+    {
+        "id": "2",
+        "firstName": "Baby",
+        "lastName" : "Das",
+        "email" : "baby@gmail.com",
+        "salary" : "70000",
+        "date" : "2021-02-14"
+    },
+    {
+        "id": "3",
+        "firstName": "Rohit",
+        "lastName" : "Verma",
+        "email" : "rohit@gmail.com",
+        "salary" : "100000",
+        "date" : "2022-07-02"
+    },
+    {
+        "id": "4",
+        "firstName": "Sandeep",
+        "lastName" : "Rathod",
+        "email" : "sandeep@gmail.com",
+        "salary" : "90000",
+        "date" : "2022-06-10"
+    },
+    {
+        "id": "5",
+        "firstName": "Jay",
+        "lastName" : "Banik",
+        "email" : "jay@gmail.com",
+        "salary" : "150000",
+        "date" : "2021-11-19"
+    }
+]
---
+++ index.js
@@ -0,0 +1,79 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
+import cors from "cors";
+dotenv.config();
+
+
+const app = express();
+// const PORT = 4000;
+const PORT = process.env.PORT;
+
+
+app.use(express.json());
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions));
+// app.use(cors);
+const MONGO_URL = process.env.MONGO_URL;
+// const MONGO_URL = "mongodb+srv://Onkar:F5Z6B0KarUxuqzhJ@cluster0.roxwh.mongodb.net";
+async function createConnection() {
+  const client = await new MongoClient(MONGO_URL);
+  client.connect();
+  console.log("Mongo is Connected😁 😊");
+  return client;
+}
+const client = await createConnection();
+
+app.get("/", function (req, res) {
+    res.send("Welcome to our World");
+  });
+
+app.get("/employees",async function (req, res) {
+    const movies = await client
+    .db("b32we")
+    .collection("movies")
+    .find({})
+    .toArray();
+    res.send(movies);
+
+});
+
+app.put("/employees/:id", async function (req, res) {
+    const { id } = req.params;
+    const bigd= req.body;
+    const nanu = await client
+    .db("b32we")
+    .collection("movies")
+    .updateOne({ id: id }, { $set: bigd });
+    res.send(nanu);
+
+});
+
+app.get("/movies/:id", async function (req, res) {
+  const { id } = req.params;
+  console.log(req.params, id);
+
+  const employee = await client
+    .db("b32we")
+    .collection("employees")
+    .findOne({ id: id });
+    employee ? res.send(employee) : res.send({ Msg: "Movie not found" });
+});
+
+app.post("/employees", express.json(), async function (req, res) {
+  const data = req.body;
+  console.log(data);
+
+  const result = await client
+    .db("b32we")
+    .collection("employees")
+    .insertMany(data);
+  res.send(result);
+});
+app.listen(PORT, () => console.log(`App Started in ${PORT}`));
+
